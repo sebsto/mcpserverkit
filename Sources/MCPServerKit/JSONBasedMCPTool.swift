270b8483ddd75e537610633b9cb168ffbb433376
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-04-06 14:50:16 +0200

    remove Any

--- Sources/MCPServerKit/JSONBasedMCPTool.swift
+++ Sources/MCPServerKit/JSONBasedMCPTool.swift
@@ -13,14 +13,14 @@ public protocol JSONBasedMCPTool: Sendable {
     var inputSchema: String { get }
     
     /// Handle JSON input and return a result that can be converted to JSON
-    func handle(jsonInput: CallTool.Parameters) async throws -> Any
+    func handle(jsonInput: CallTool.Parameters) async throws -> Encodable
 }
 
 /// An adapter that wraps any MCPToolProtocol to make it conform to JSONBasedMCPTool
-public struct JSONMCPToolAdapter<I, O>: JSONBasedMCPTool {
+public struct JSONMCPToolAdapter<I, O: Encodable>: JSONBasedMCPTool {
     private let tool: any MCPToolProtocol<I, O>
     
-    public init(_ tool: any MCPToolProtocol<I, O>) {
+    public init(_ tool: any MCPToolProtocol<I, O>) where O: Encodable {
         self.tool = tool
     }
     
@@ -28,7 +28,7 @@ public struct JSONMCPToolAdapter<I, O>: JSONBasedMCPTool {
     public var description: String { tool.description }
     public var inputSchema: String { tool.inputSchema }
     
-    public func handle(jsonInput: CallTool.Parameters) async throws -> Any {
+    public func handle(jsonInput: CallTool.Parameters) async throws -> Encodable {
         // Convert JSON input to the tool's input type
         let input = try await tool.convert(jsonInput)
         
@@ -41,7 +41,7 @@ public struct JSONMCPToolAdapter<I, O>: JSONBasedMCPTool {
 }
 
 // Extension to easily create a JSON-based tool from an MCPTool
-extension MCPToolProtocol {
+extension MCPToolProtocol where Output: Encodable {
     public func asJSONTool() -> JSONBasedMCPTool {
         return JSONMCPToolAdapter(self)
     }
--- Sources/MCPServerKit/MCPServer.swift
+++ Sources/MCPServerKit/MCPServer.swift
@@ -1,80 +1,80 @@
-import MCP
-
-#if canImport(FoundationEssentials)
-import FoundatioNEssentials
-#else
-import Foundation
-#endif
-
-//TODO: add compliance to Swift Service Lifecycle
-//TODO: add a logger into the game
-public struct MCPServer<Input: Decodable, Output>: Sendable {
-
-    let name: String
-    let version: String
-    let tools: [any MCPToolProtocol<Input, Output>]
-
-    public init(
-        name: String,
-        version: String,
-        tools: [any MCPToolProtocol<Input, Output>]
-    ) {
-        self.name = name
-        self.version = version
-        self.tools = tools
-    }
-
-    public func startStdioServer() async throws {
-
-        // create the server
-        let server = Server(
-            name: name,
-            version: version,
-            capabilities: .init(
-                tools: .init()
-            )
-        )
-
-        // register the tools, part 1 : tools/list
-        await server.withMethodHandler(ListTools.self) { params in
-
-            let _tools = try tools.map { tool in
-                Tool(
-                    name: tool.name,
-                    description: tool.description,
-                    inputSchema: try JSONDecoder().decode(
-                        Value.self,
-                        from: tool.inputSchema.data(using: .utf8)!
-                    )
-                )
-            }
-
-            return ListTools.Result(tools: _tools, nextCursor: nil)
-        }
-
-        // register the tools, part 2 : tools/call
-        await server.withMethodHandler(CallTool.self) { params in
-
-            // Check if the tool name is in our list of tools
-            guard let tool = tools.first(where: { $0.name == params.name }) else {
-                throw MCPServerError.unknownTool(params.name)
-            }
-
-            // the tool converts the parameters to its expected input type
-            let input = try await tool.convert(params)
-
-            // call the tool
-            let output = try await tool.handler(input)
-
-            // return the result
-            //FIXME: should we return the output as JSON object?
-            return CallTool.Result(content: [.text(String(describing: output))])
-        }
-
-        // start the server with the stdio transport
-        try await server.start(transport: StdioTransport())
-
-        await server.waitUntilCompleted()
-
-    }
-}
+//import MCP
+//
+//#if canImport(FoundationEssentials)
+//import FoundatioNEssentials
+//#else
+//import Foundation
+//#endif
+//
+////TODO: add compliance to Swift Service Lifecycle
+////TODO: add a logger into the game
+//public struct MCPServer<Input: Decodable, Output>: Sendable {
+//
+//    let name: String
+//    let version: String
+//    let tools: [any MCPToolProtocol<Input, Output>]
+//
+//    public init(
+//        name: String,
+//        version: String,
+//        tools: [any MCPToolProtocol<Input, Output>]
+//    ) {
+//        self.name = name
+//        self.version = version
+//        self.tools = tools
+//    }
+//
+//    public func startStdioServer() async throws {
+//
+//        // create the server
+//        let server = Server(
+//            name: name,
+//            version: version,
+//            capabilities: .init(
+//                tools: .init()
+//            )
+//        )
+//
+//        // register the tools, part 1 : tools/list
+//        await server.withMethodHandler(ListTools.self) { params in
+//
+//            let _tools = try tools.map { tool in
+//                Tool(
+//                    name: tool.name,
+//                    description: tool.description,
+//                    inputSchema: try JSONDecoder().decode(
+//                        Value.self,
+//                        from: tool.inputSchema.data(using: .utf8)!
+//                    )
+//                )
+//            }
+//
+//            return ListTools.Result(tools: _tools, nextCursor: nil)
+//        }
+//
+//        // register the tools, part 2 : tools/call
+//        await server.withMethodHandler(CallTool.self) { params in
+//
+//            // Check if the tool name is in our list of tools
+//            guard let tool = tools.first(where: { $0.name == params.name }) else {
+//                throw MCPServerError.unknownTool(params.name)
+//            }
+//
+//            // the tool converts the parameters to its expected input type
+//            let input = try await tool.convert(params)
+//
+//            // call the tool
+//            let output = try await tool.handler(input)
+//
+//            // return the result
+//            //FIXME: should we return the output as JSON object?
+//            return CallTool.Result(content: [.text(String(describing: output))])
+//        }
+//
+//        // start the server with the stdio transport
+//        try await server.start(transport: StdioTransport())
+//
+//        await server.waitUntilCompleted()
+//
+//    }
+//}
