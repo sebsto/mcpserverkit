5866cf3b04b62621e50ca0543d3b707a78e79ca4
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-06-27 18:00:57 +0200

    improve comments

--- Sources/Macro/ServerMacroClient/server+macro.swift
+++ Sources/Macro/ServerMacroClient/server+macro.swift
@@ -2,6 +2,7 @@ import MCPServerKit
 import ToolMacro
 import ServerMacro
 
+// create a tool
 @Tool(name: "Hello", description: "Say hello")
 struct SayHello: MCPToolProtocol {
     /// Say Hello
@@ -9,7 +10,7 @@ struct SayHello: MCPToolProtocol {
     func handler(input: String) async throws -> String { return "Hello \(input)" }
 }
 
-// Create a simple struct to hold our server
+// Create a server
 @Server(name: "Say Hello Server",
         version: "1.0.0",
         description: "A simple server that says hello",
